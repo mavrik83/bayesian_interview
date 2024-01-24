dcec28c43a01b9d4adb5f1fb5286af8c76d0bcd7
Ryan Huber <rjhuber83@gmail.com>
2024-01-24 12:14:53 -0500

    adds readme

--- api/prisma/migrations/20240124171019_init/migration.sql
+++ api/prisma/migrations/20240124171019_init/migration.sql
@@ -8,7 +8,7 @@ CREATE TABLE "artists" (
 CREATE TABLE "albums" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "release_date" TEXT NOT NULL,
+    "release_date" DATETIME NOT NULL,
     "price" REAL NOT NULL,
     "artist_id" INTEGER,
     CONSTRAINT "albums_artist_id_fkey" FOREIGN KEY ("artist_id") REFERENCES "artists" ("id") ON DELETE SET NULL ON UPDATE CASCADE
@@ -19,5 +19,7 @@ CREATE TABLE "songs" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "artist_id" INTEGER,
-    CONSTRAINT "songs_artist_id_fkey" FOREIGN KEY ("artist_id") REFERENCES "artists" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    "albumId" INTEGER,
+    CONSTRAINT "songs_artist_id_fkey" FOREIGN KEY ("artist_id") REFERENCES "artists" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "songs_albumId_fkey" FOREIGN KEY ("albumId") REFERENCES "albums" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
